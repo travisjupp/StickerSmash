79abe22f7e433b0d74037b115470b933df260e8f
tjupp <travisjupp@gmail.com>
2025-04-26 10:32:00 -0600

    Fix styles syntax error

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,25 +15,26 @@ export default function Button({ label }: Props) {
 }
 
 const styles = StyleSheet.create(
-  buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
-  },
-  button: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  buttonLabel: {
-    color: '#fff',
-    fontSize: 16,
-  },
-);
+  {
+    buttonContainer: {
+      width: 320,
+      height: 68,
+      marginHorizontal: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 3,
+    },
+    button: {
+      borderRadius: 10,
+      width: '100%',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row',
+    },
+    buttonLabel: {
+      color: '#fff',
+      fontSize: 16,
+    },
+  });
 
