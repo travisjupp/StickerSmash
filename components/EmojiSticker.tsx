32e37e4323fc5b10b956acb00e6dffa37f68d1c5
tjupp <travisjupp@gmail.com>
2025-04-29 11:11:18 -0600

    Add a pan gesture
    Update the <EmojiSticker> component so that the <GestureDetector>
    component becomes the top-level component. Add the containerStyle on the
    <Animated.View> component to apply the transform styles.

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -54,15 +54,17 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   });
 
   return (
-    <Animated.View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-      <Animated.Image 
-        source={stickerSource} 
-        resizeMode="contain"
-        style={[imageStyle, { width: imageSize, height: imageSize } ]} 
-      />
-      </GestureDetector>
-    </Animated.View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 } ]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image 
+            source={stickerSource} 
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize } ]} 
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
 
