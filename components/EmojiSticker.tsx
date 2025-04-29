0e2a1f80e29cbf99ecc051737dfab3d521ce534c
tjupp <travisjupp@gmail.com>
2025-04-29 10:23:22 -0600

    Add a tap gesture
    We'll create a doubleTap object to scale the initial value and use
    Gesture.Tap() to animate the transition while scaling the sticker image.
    To determine the number of taps required, we'll add numberOfTaps().

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -10,6 +10,17 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+
+  const doubleTap = Gesture.Tap()
+  .numberOfTaps(2)
+  .onStart(() => {
+    if (scaleImage.value !== imageSize * 2) {
+      scaleImage.value = scaleImage.value * 2;
+    } else {
+      scaleImage.value = Math.round(scaleImage.value / 2);
+    }
+  });
+
   return (
     <View style={{ top: -350 }}>
       <Animated.Image 
