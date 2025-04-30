b60dfcc63c90b87e7429182691dfe2e3ba75d75d
tjupp <travisjupp@gmail.com>
2025-04-30 13:13:07 -0600

    Configure the status bar
    expo-status-bar library comes pre-installed in every project created
    using create-expo-app. This library provides a StatusBar component to
    configure the app's status bar style.
    
    Inside app/_layout.tsx:
    
    1. Import StatusBar from expo-status-bar.
    
    2. Group the StatusBar and existing Stack components with React's
    Fragment component.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
 // ROOT LAYOUT FILE
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* ROUTES */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <StatusBar style="auto" />
+      <Stack>
+        {/* ROUTES */}
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </>
   );
 }
