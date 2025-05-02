3e282fd55a9749f3d69dbbcf1928f70a658e7122
tjupp <travisjupp@gmail.com>
2025-05-02 16:59:32 -0600

    Configure multiple app variants
    UPDATE DYNAMIC VALUES BASED ON ENVIRONMENT
    
    To identify the build type, let's add two environment variables called
    IS_DEV andIS_PREVIEW for development and preview build profiles in
    app.config.js
    
    Then, add two functions that dynamically change the app
    name, Android Application ID and iOS Bundle Identifier
    
    We'll use getAppName() to assign dynamic name values for the app and
    getUniqueIdentifier() to differentiate android.package and
    ios.bundleIdentifier for development and preview builds:

--- app/app.config.js
+++ app/app.config.js
@@ -1,3 +1,39 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.tjupp.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.tjupp.stickersmash.preview';
+  }
+
+  return 'com.tjupp.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmask: Emoji Stickers';
+};
+
 export default ({ config }) => {
   ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
 });
